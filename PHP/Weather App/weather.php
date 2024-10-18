6d07668ac461931e320383c86275875edb9af43c
DilankaDI <dilshandilanka96@gmail.com>
2024-10-18 11:25:16 +0530

    Weather app php
    All set and working fine

---
+++ PHP/Weather App/weather.php
@@ -0,0 +1,46 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weather App</title>
+    <link rel="stylesheet" href="styles.css">    
+</head>
+
+<body>
+  <div id="square">
+    <div id="moon">
+      <div id="hole1" class="hole"></div>
+      <div id="hole2" class="hole"></div>
+      <div id="hole3" class="hole"></div>
+      <div id="hole4" class="hole"></div>
+      <div id="hole5" class="hole"></div>
+      <div id="hole6" class="hole"></div>
+      <div id="hole7" class="hole"></div>
+    </div>
+    <div id="floor1" class="floor light"></div>
+    <div id="floor2" class="floor light"></div>
+    <div id="floor3" class="floor light"></div>
+    <div id="floor4" class="floor dark"></div>
+    <div id="floor5" class="floor dark"></div>
+    <div id="info">
+      <span id="temp">25°</span>
+      <div id="stats">
+        <span>Wind: NE 21Km/h</span>
+        <span>Humidity: 50%</span>
+      </div>
+      <div id="next">
+        <span>FRI<b>23° / 11°</b></span>
+        <span>SAT<b>25° / 15°</b></span>
+      </div>
+    </div>
+    <div id="drop1" class="drop"></div>
+    <div id="drop2" class="drop"></div>
+    <div id="drop3" class="drop"></div>
+    <div id="drop4" class="drop"></div>
+    <div id="drop5" class="drop away"></div>
+    <div id="drop6" class="drop away"></div>
+    <div id="drop7" class="drop away"></div>
+    <div id="drop8" class="drop away"></div>
+  </div>
+</body>
+</html>
