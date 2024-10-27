7e6c1171457919d04ed14e641dbada81e41d5f0f
Abhi8756 <Abhijit.potty2023@vitstudent.ac.in>
2024-10-27 20:53:40 +0530

    complete overhaul of rock paper scissors

--- HTML/Rock, Paper, Scissors/script.js
+++ HTML/Rock, Paper, Scissors/script.js
@@ -4,21 +4,28 @@ let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const resultMessage_p = document.getElementById("result-message");
+const computerChoice_p = document.getElementById("computer-choice");
+const userChoice_p = document.getElementById("user-choice");
 
 const choices = document.querySelectorAll(".choices button");
 
 choices.forEach(choice => {
     choice.addEventListener("click", function() {
         const userChoice = this.id;
+        userChoice_p.innerText = "User chose: " + userChoice;
         const computerChoice = getComputerChoice();
         determineWinner(userChoice, computerChoice);
     });
 });
 
+
+
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * 3);
-    return choices[randomIndex];
+    const output = choices[randomIndex];
+    computerChoice_p.innerText = "Computer chose: " + output;
+    return output;
 }
 
 function determineWinner(user, computer) {
